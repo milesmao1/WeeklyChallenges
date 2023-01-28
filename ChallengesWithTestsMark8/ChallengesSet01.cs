e3f36bef956ef45103ddf4505d1a414166f9c2df
Miles Mao <mrmao1@gmail.com>
2023-01-28 14:02:41 -0600

    Challenges Set1 results

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,44 +6,75 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            //if(num1 == num2)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return (num1 == num2) ? true: false;
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+           // throw new NotImplementedException();
+
+            var sum =  number1 + number2;
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            //if (number1 < number2) 
+            //{
+            //    return number1;
+            //}
+            //else 
+            //{
+            //    return number2;
+            //}
+
+            return (number1 < number2) ? number1 : number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            //if (nameOfPerson != "") 
+            //{
+            //    return $"Hello, {nameOfPerson}!";
+            //}
+            //else 
+            //{
+            //    return "Hello!";
+            //}
+
+            return (nameOfPerson != "") ? $"Hello, {nameOfPerson}!" : "Hello!";
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
