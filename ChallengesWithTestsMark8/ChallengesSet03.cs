3e39af90b2f9c55d8d57fb1837ff584e59700f21
Miles Mao <mrmao1@gmail.com>
2023-01-31 13:16:33 -0600

    Weekly Challenges set03 results

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +9,137 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            //for (var i = 0; i < vals.Length; i++) 
+            // { 
+            //     if (vals[i] == false) 
+            //     {
+            //         return true;
+            //     }
+            // }
+            //return false;
+
+            return vals.Contains(false);
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            //if (numbers == null)
+            //{
+            //    return false;
+            //}
+
+            //var odds  = new List<int>();
+
+            //foreach (var num in numbers) 
+            //{
+            //    if(num%2 != 0) 
+            //    {
+            //        odds.Add(num);
+            //    }
+            //}
+            //var sumOdds = odds.Sum();
+            //return (sumOdds %2 != 0);
+
+            return (numbers == null) ? false : (numbers.Where(x => x % 2 != 0).Sum() % 2 != 0);
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            var isLower = false;
+            var isUpper = false;
+            var isNumber = false;
+
+            //for (var i = 0; i < password.Length; i++)
+            //{
+            //    if (char.IsLower(password[i]))
+            //    {
+            //        isLower = true;
+            //    }
+            //    if (char.IsUpper(password[i]))
+            //    {
+            //        isUpper = true;
+            //    }
+            //    if (char.IsNumber(password[i])) 
+            //    {
+            //        isNumber = true;
+            //    }
+            //}
+
+            //if (isLower == true && isUpper == true && isNumber == true)
+            //{
+            //    return true;
+            //}
+
+            //else 
+            //{
+            //    return false;
+            // }
+            return (password.Any(char.IsLower)
+                && password.Any(char.IsUpper)
+                && password.Any(char.IsNumber));
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            // return val[0];
+            return val.First();
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            //return val[val.Length - 1];
+
+            //return val.Last();
+
+            return val[^1];
         }
+
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            //if(divisor == 0) 
+            //{
+            //    return 0;
+            //}
+            //    return dividend / divisor;
+
+            return (divisor == 0) ? 0 : dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            //return nums[nums.Length - 1] - nums[0];
+
+            //return nums[^1] - nums[0];
+
+            return nums.Last() - nums.First();
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+        //    var list = new List<int>();
+
+        //    for (var i = 0; i < 100; i++) 
+        //    {
+        //        if(i % 2 != 0) 
+        //        {
+        //            list.Add(i);
+        //        }
+        //    }
+        //    return list.ToArray();
+
+            return Enumerable.Range(0, 100).Where(x => x % 2 != 0).ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < words.Length; i++) 
+            {
+                words [i] = words[i].ToUpper();
+            }
         }
     }
 }
